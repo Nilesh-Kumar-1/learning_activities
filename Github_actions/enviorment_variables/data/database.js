9d5b465b647fbe5471d967ec59f29e2759a56d92
Nilesh-Kumar-1 <nk1880571@gmail.com>
2024-10-13 22:59:43 +0530

    updates

--- Github_actions/enviorment_variables/data/database.js
+++ Github_actions/enviorment_variables/data/database.js
@@ -5,18 +5,18 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
 try {
-  await client.connect();
-  await client.db(dbName).command({ ping: 1 });
+  client.connect();
+  client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
   console.log('Connection failed.');
-  await client.close();
+  client.close();
   console.log('Connection closed.');
 }
 
