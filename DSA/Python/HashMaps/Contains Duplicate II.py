75e680ee0f8fc57efa544357b77b06bb1143745f
Nilesh-Kumar-1 <nk1880571@gmail.com>
2024-12-31 13:50:52 +0530

    DSA - Python: Maps Contains duplicates 2

--- DSA/Python/HashMaps/Contains Duplicate II.py
+++ DSA/Python/HashMaps/Contains Duplicate II.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
         Map = {}
         for i in range(len(nums)):
             if nums[i] in Map and abs(i - Map[nums[i]]) <= k:
